85469b67e9c50876785aadc7a3b4d1faf9230928
aneuno <155757270+aneuno@users.noreply.github.com>
2026-09-02 19:45:15 +0200

    Add character randomizer functionality
    Fetch characters from JSON and display a random name on button click.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const button = document.getElementById("button");
 
+let result = document.getElementById("result");
+let characters = [];
+
+fetch("characters.json")
+    .then(response => response.json())
+    .then(data => {
+        characters = data.characters;
+        console.log(characters);
+    });
+
+button.onclick = function() {
+    let i = Math.floor(Math.random() * characters.length);
+    result.textContent = characters[i].name;
+}
