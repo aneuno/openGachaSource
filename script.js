8e01c0956aaab0e80fbe9b750e5ae1618925d2eb
aneuno <155757270+aneuno@users.noreply.github.com>
2026-09-03 22:06:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const SUPABASE_URL = "TON_PROJECT_URL";
+const SUPABASE_KEY = "TA_PUBLISHABLE_KEY";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+
 const button = document.getElementById("summonButton");
 const multiButton = document.getElementById("multiSummonButton");
 
@@ -6,22 +15,30 @@ const resultImage = document.getElementById("resultImage");
 
 let characters = [];
 
+
 fetch("characters.json")
     .then(response => response.json())
     .then(data => {
         characters = data.characters;
+
         console.log("Personnages chargés :", characters);
     })
-    .catch(error => console.error("Erreur lors du chargement :", error));
+    .catch(error => {
+        console.error("Erreur lors du chargement :", error);
+    });
+
 
 function getRandomCharacter() {
+
     const totalWeight = characters.reduce(
-        (total, character) => total + character.weight, 0
+        (total, character) => total + character.weight,
+        0
     );
 
     let random = Math.random() * totalWeight;
 
     for (const character of characters) {
+
         random -= character.weight;
 
         if (random < 0) {
@@ -32,38 +49,88 @@ function getRandomCharacter() {
     return null;
 }
 
+
 function summon() {
-    if (characters.length === 0) return;
+
+    if (characters.length === 0) {
+        return;
+    }
 
     const chosen = getRandomCharacter();
 
-    if (!chosen) return;
+    if (!chosen) {
+        return;
+    }
+
 
     result.textContent = chosen.name;
 
+
     if (chosen.image) {
+
         resultImage.referrerPolicy = "no-referrer";
+
         resultImage.src = chosen.image;
+
         resultImage.alt = chosen.name;
+
         resultImage.style.display = "block";
+
         resultImage.classList.remove("enlarged");
+
     } else {
+
         resultImage.style.display = "none";
+
     }
+
 
     console.log("Personnage obtenu :", chosen.name);
     console.log("Rareté :", chosen.rarity);
     console.log("Weight :", chosen.weight);
 }
 
+
 button.onclick = function() {
     summon();
 };
 
+
 multiButton.onclick = function() {
+
     console.log("10 summon +1");
+
 };
 
+
 resultImage.onclick = function() {
+
     resultImage.classList.toggle("enlarged");
+
 };
+
+
+async function testSupabase() {
+
+    const { data, error } = await supabaseClient.auth.getSession();
+
+    if (error) {
+
+        console.error(
+            "Erreur Supabase :",
+            error
+        );
+
+        return;
+    }
+
+    console.log("Supabase fonctionne !");
+
+    console.log(
+        "Session :",
+        data.session
+    );
+}
+
+
+testSupabase();
